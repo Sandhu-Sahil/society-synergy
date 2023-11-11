e795ca1eed7dac47ea7a45c495d4893ea540cf81
Sandhu-Sahil <sandhu.sahil2002@gmail.com>
2023-11-11 23:24:15 +0530

    WORST MISTAKE BY ME FUCKKKK

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,6 @@
 const url = //"http://13.50.5.140:3003";
 // "http://localhost:3003";
-"api.society-synergy.nl";
-const hosturl = "https://society-synergy.netlify.app/";
+"https://api.society-synergy.nl";
+const hosturl = "https://society-synergy.netlify.app";
 const hosturl2 = "https://society-synergy.one";
 export default url;
